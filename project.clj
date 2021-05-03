6eeb426b6277d651625df49f4a84b0738dda9bae
Bradyn Glines <glinesbdev@gmail.com>
2021-05-02 21:44:15 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fennel-workflow "0.1.0-SNAPSHOT"
+(defproject fennel-workflow "0.1.0"
   :description "A Fennel to Lua workflow"
   :url "https://github.com/glinesbdev/fennel-workflow"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
